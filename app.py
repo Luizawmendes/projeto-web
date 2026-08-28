b50a1249783858b0e17daa99ac95211a25ee9098
Maria Luiza dos Santos Mendes <luizawmens@outlook.com>
2026-08-27 22:10:47 -0300

    credenciais do github

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route('/')
+def pagina_inicial():
+    return '<h1> Olá </h1>'
+
+@app.route('/nome')
+def nome():
+    return '<p> Luiza Mendes </p>'
+
+if __name__=='__main__':
+    app.run(debug=True)
+    
